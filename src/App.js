bdbac70643ecd88537997ea1de82d48f5e65f9e1
zahwaihab <zahwa1703409@miuegypt.edu.eg>
2023-05-08 23:14:19 +0300

    usestate code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const styleObject={
+    color: "teal",
+    fontStyle: "italic",
+    justify: "center",
+  };
+
+  let Person= {
+    fullName: "Andrew Maleek",
+    bio: "bio",
+    profession: "profession",
+  }
+
+  let [isChecked , SetChecked]= useState(false);
+  let [personState , setState]=useState("notvisibile")
+
+  function handleChecked(){
+    SetChecked(!isChecked);
+   }
+  
+   let togglePersonDetails=isChecked?"active ":"notactive";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <h1 style={styleObject} >Welcome to use state hooks</h1>
+     <div onchange={() => setState("visible")} className={togglePersonDetails}>
+       <p>{Person.fullName}</p>
+       <p>{Person.bio}</p>
+       <p>{Person.profession}</p>
+     </div>
+     <input type="checkbox"  checked={isChecked} onChange={handleChecked} ></input>
+     <label>{isChecked? "Checked" : "NotChecked"}</label>
+
+    </>
   );
 }
 
